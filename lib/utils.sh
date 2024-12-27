89ea592c2111ac9792e7cf1348b9dc6e84f06d3a
Mark Lee <asdf@lazymalevolence.com>
2024-12-27 10:34:04 -0800

    feat: allow installing Linux binaries from rvm.io or ruby/ruby-builder

--- lib/utils.sh
+++ lib/utils.sh
@@ -76,3 +76,100 @@ ruby_build_source_dir() {
 ruby_build_path() {
   echo "$(ruby_build_dir)/bin/ruby-build"
 }
+
+ensure_patchelf_installed() {
+  if ! command -v patchelf >/dev/null; then
+    errorexit "Run 'sudo apt install patchelf' to install Ruby binaries from ruby/ruby-builder"
+  fi
+}
+
+load_os_release() {
+  local os_release
+
+  # Needed for both the distro ID and version ID
+  test -e /etc/os-release && os_release='/etc/os-release' || os_release='/usr/lib/os-release'
+  # shellcheck source=/dev/null
+  source "${os_release}"
+}
+
+get_rvm_io_linux_distro() {
+  local distro_slug distro_id
+
+  distro_id="$1"
+
+  if [[ -n "${RUBY_INSTALL_BINARY_RVM_DISTRO:-}" ]]; then
+    echo "$RUBY_INSTALL_BINARY_RVM_DISTRO"
+    return
+  fi
+
+  case "$distro_id" in
+  amzn) distro_slug="amazon" ;;
+  arch | centos | debian | opensuse | ubuntu) distro_slug="$distro_id" ;;
+  ol) distro_slug="oracle" ;;
+  "opensuse-leap") distro_slug="opensuse" ;;
+  *) errorexit "Unsupported Linux distro $NAME, install from source instead" ;;
+  esac
+
+  echo "$distro_slug"
+}
+
+get_rvm_io_url() {
+  local kernel
+  kernel="$(uname -s)"
+  case "$kernel" in
+  Darwin)
+    # There is an osx folder but the binaries haven't been built since 2015
+    errorexit "macOS not supported by rvm.io/binaries, install from source instead"
+    ;;
+
+  Linux)
+    local distro
+    load_os_release
+    distro="$(get_rvm_io_linux_distro "$ID")"
+    echo "https://rvm.io/binaries/$distro/${VERSION_ID:-UNDEFINED_BY_OS_RELEASE}/$(uname -m)"
+    ;;
+
+  *) errorexit "OS '$kernel' not supported by rvm.io/binaries, install from source instead" ;;
+  esac
+}
+
+run_gnu_tar() {
+  local tar
+  case "$(uname -s)" in
+  Darwin) tar=gtar ;;
+  *) tar=tar ;;
+  esac
+
+  "$tar" "$@"
+}
+
+download_and_install_prebuilt_ruby() {
+  local base_url download_file filename install_path url
+  base_url="$1"
+  filename="$2"
+  install_path="$3"
+  url="$base_url/$filename"
+  download_file="$(mktemp -d "${TMPDIR:-/tmp}/asdf-ruby.XXXXXX")/$filename"
+
+  curl --fail --silent --show-error --location --output "$download_file" "$url"
+  mkdir -p "$install_path"
+  run_gnu_tar --strip-components=1 --extract --file="$download_file" --directory="$install_path" --preserve-permissions
+}
+
+fix_runpath() {
+  local kernel="$1"
+  local install_path="$2"
+  case "$kernel" in
+  Linux)
+    # shellcheck disable=SC2016
+    patchelf --set-rpath '${ORIGIN}/../lib' "$install_path/bin/ruby"
+    # shellcheck disable=SC2016
+    find "$install_path" -type f -name "*.so" -exec patchelf --set-rpath '${ORIGIN}/../lib' "{}" \;
+    ;;
+  Darwin)
+    # TODO: run install_name_tool on the ruby executable plus any libraries (*.bundle?)
+    # to change the rpath of libruby*.dylib to @loader_path/../lib/libruby*.dylib
+    errorexit "fix_runpath not implemented for macOS"
+    ;;
+  esac
+}
